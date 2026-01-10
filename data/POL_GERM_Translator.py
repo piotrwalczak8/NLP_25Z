cf8d3ecbca979934b01f7bcd59c348bc8594d5ca
Bartosz Zaborowski <bartoszzaborowski@macbook-air-bartosz.home>
2026-01-10 18:40:24 +0100

    pip requirements addup, gitignore fixup, translation file addup

---
+++ data/POL_GERM_Translator.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from deep_translator import GoogleTranslator
+from tqdm import tqdm
+
+# Włączamy pasek postępu
+tqdm.pandas()
+
+# Wczytanie oryginalnego CSV
+df = pd.read_csv("tweets.csv")
+
+# Funkcja tłumaczenia
+def translate_to_german(text):
+    try:
+        return GoogleTranslator(source='auto', target='de').translate(text)
+    except Exception as e:
+        print(f"Nie udało się przetłumaczyć: {text}\nBłąd: {e}")
+        return text 
+
+# Tłumaczenie kolumny 'text'
+df['text_de'] = df['text'].progress_apply(translate_to_german)
+
+# Zapis nowego CSV
+df.to_csv("tweets_german.csv", index=False)
+
+print("Tłumaczenie zakończone, zapisano do tweets_german.csv")
